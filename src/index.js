112370b3cd444c7024a8d2ef80b16acc3edbaa16
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-12-29 19:16:21 +0200

    Change hardcoded port in index.js

--- src/index.js
+++ src/index.js
@@ -69,6 +69,5 @@ app.use((err, req, res, next) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
