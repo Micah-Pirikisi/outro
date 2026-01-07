48667d1ae51115551d2ea3062764157f8649ee8b
Micah-Pirkisi <micahpirikisi@gmail.com>
2026-01-07 21:55:38 +0200

    Removed duplicate titles

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -37,14 +37,24 @@ export const postPage = async (req, res, next) => {
     try {
       const h1Regex = new RegExp('^\\s*<h1[^>]*>\\s*' + escapeRegExp(title) + '\\s*</h1>', 'i');
       if (h1Regex.test(displayContent)) displayContent = displayContent.replace(h1Regex, '');
-      // Remove a plain text title at start
-      const plainTitleRegex = new RegExp('^\\s*' + escapeRegExp(title) + '\\s*', 'i');
-      if (plainTitleRegex.test(displayContent)) displayContent = displayContent.replace(plainTitleRegex, '');
-      // Remove leading "by Author" lines
-      if (authorName) {
-        const byAuthorRegex = new RegExp('^\\s*(by\\s+' + escapeRegExp(authorName) + ')[\\s\\S]{0,200}?', 'i');
-        displayContent = displayContent.replace(byAuthorRegex, '');
-      }
+        // Remove a plain text title at start
+        const plainTitleRegex = new RegExp('^\\s*' + escapeRegExp(title) + '\\s*', 'i');
+        if (plainTitleRegex.test(displayContent)) displayContent = displayContent.replace(plainTitleRegex, '');
+        // Remove leading "by Author" lines
+        if (authorName) {
+          const byAuthorRegex = new RegExp('^\\s*(by\\s+' + escapeRegExp(authorName) + ')[\\s\\S]{0,200}?', 'i');
+          displayContent = displayContent.replace(byAuthorRegex, '');
+        }
+        // Remove any duplicated title that appears again near the start of the content
+        try {
+          const prefix = displayContent.slice(0, 300).toLowerCase();
+          if (title && prefix.includes(title.toLowerCase())) {
+            const dupRegex = new RegExp(escapeRegExp(title), 'i');
+            displayContent = displayContent.replace(dupRegex, '');
+          }
+        } catch (e) {
+          // ignore
+        }
     } catch (e) {
       // if any regex fails, fall back to original content
       displayContent = post.content;
