b0ee820a4fb17d53921a4d7cbac1e42ad5e60ada
Micah-Pirkisi <micahpirikisi@gmail.com>
2026-03-16 11:25:13 +0200

    Add mediaSrc to CSP for audio support

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ app.use(
         scriptSrc: ["'self'"],
         styleSrc: ["'self'", "'unsafe-inline'"],
         scriptSrcAttr: ["'self'"],
+        mediaSrc: ["'self'"],
       },
     },
   }),
